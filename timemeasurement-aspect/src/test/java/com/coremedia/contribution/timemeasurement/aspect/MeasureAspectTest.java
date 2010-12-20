f64c0289dbf04d2b09dbccaa5214078ed787c25f
afranken <afranken@0b772442-175c-4413-812b-a38e8c6abb97>
2010-12-20 10:33:41 +0000

    added test for the Aspect
    git-svn-id: https://contributions.coremedia.com/svn-shared/timemeasurement/trunk@12 0b772442-175c-4413-812b-a38e8c6abb97

---
+++ timemeasurement-aspect/src/test/java/com/coremedia/contribution/timemeasurement/aspect/MeasureAspectTest.java
@@ -0,0 +1,58 @@
+package com.coremedia.contribution.timemeasurement.aspect;
+
+import com.coremedia.contribution.timemeasurement.annotation.Measure;
+import com.coremedia.contributions.timemeasurement.TimeMeasurement;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class MeasureAspectTest
+{
+	@Test
+	public void testMeasureAspect()
+	{
+		System.setProperty("timemeasurement.enabled","true");
+
+		consumeTime();
+
+		assertTrue(TimeMeasurement.getMeasurementResults().contains("consumeTime"));
+	}
+
+	@Test
+	public void testMeasureAspectException()
+	{
+		System.setProperty("timemeasurement.enabled","true");
+
+		try
+		{
+			consumeTimeException();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		assertTrue(TimeMeasurement.getMeasurementResults().contains("consumeTimeException"));
+	}
+
+	@Measure("consumeTimeException")
+	public void consumeTimeException() throws Exception
+	{
+		Thread.sleep(100);
+
+		throw new Exception();
+	}
+
+	@Measure("consumeTime")
+	public void consumeTime()
+	{
+		try
+		{
+			Thread.sleep(100);
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}

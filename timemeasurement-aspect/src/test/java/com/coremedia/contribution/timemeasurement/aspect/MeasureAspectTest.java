45d2f9d44a4b629cb5acf9eafa3fe91915cbfffa
Arne Franken <arne.franken@coremedia.com>
2015-03-09 09:29:19 +0100

    formatting, ignore exception in test.

--- timemeasurement-aspect/src/test/java/com/coremedia/contribution/timemeasurement/aspect/MeasureAspectTest.java
+++ timemeasurement-aspect/src/test/java/com/coremedia/contribution/timemeasurement/aspect/MeasureAspectTest.java
@@ -1,58 +1,47 @@
 package com.coremedia.contribution.timemeasurement.aspect;
 
-import com.coremedia.contribution.timemeasurement.annotation.Measure;
 import com.coremedia.contribution.timemeasurement.TimeMeasurement;
+import com.coremedia.contribution.timemeasurement.annotation.Measure;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public class MeasureAspectTest
-{
-	@Test
-	public void testMeasureAspect()
-	{
-		System.setProperty("timemeasurement.enabled","true");
+public class MeasureAspectTest {
+  @Test
+  public void testMeasureAspect() {
+    System.setProperty("timemeasurement.enabled", "true");
 
-		consumeTime();
+    consumeTime();
 
-		assertTrue(TimeMeasurement.getMeasurementResults().contains("consumeTime"));
-	}
+    assertTrue(TimeMeasurement.getMeasurementResults().contains("consumeTime"));
+  }
 
-	@Test
-	public void testMeasureAspectException()
-	{
-		System.setProperty("timemeasurement.enabled","true");
+  @Test
+  public void testMeasureAspectException() {
+    System.setProperty("timemeasurement.enabled", "true");
 
-		try
-		{
-			consumeTimeException();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
+    try {
+      consumeTimeException();
+    } catch (Exception e) {
+      //ignore exception
+    }
 
-		assertTrue(TimeMeasurement.getMeasurementResults().contains("consumeTimeException"));
-	}
+    assertTrue(TimeMeasurement.getMeasurementResults().contains("consumeTimeException"));
+  }
 
-	@Measure("consumeTimeException")
-	public void consumeTimeException() throws Exception
-	{
-		Thread.sleep(100);
+  @Measure("consumeTimeException")
+  public void consumeTimeException() throws Exception {
+    Thread.sleep(100);
 
-		throw new Exception();
-	}
+    throw new Exception();
+  }
 
-	@Measure("consumeTime")
-	public void consumeTime()
-	{
-		try
-		{
-			Thread.sleep(100);
-		}
-		catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-	}
+  @Measure("consumeTime")
+  public void consumeTime() {
+    try {
+      Thread.sleep(100);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 }
